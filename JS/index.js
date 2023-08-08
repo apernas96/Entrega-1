45f0c8e3c569e79eacaae6025aba53742a5fc91f
Andres Pernas <ing.andrespernas@gmail.com>
2023-08-08 18:59:35 -0300

    para entrega

--- JS/index.js
+++ JS/index.js
@@ -1,31 +1,4 @@
-/* let nombre = prompt("Ingrese aqui su nombre:")
-let condicionCierre = false;
 
-let eleccion = prompt(`Hola ${nombre}, en que puedo ayudarte hoy? \n Las funciones permitidas son: \n 1 - \n 2 - \n 3 - \n 4 - \n 5 - Finalizar! `)
-while (!condicionCierre) {
-    let eleccion = ;
-    switch (eleccion) {
-        case 1:
-            //ingresar funcion 1
-            break;
-        case 2:
-            //ingresar funcion 1
-            break;
-        case 3:
-            //ingresar funcion 1
-            break;
-        case 4:
-            //ingresar funcion 1
-            break;
-        case 5:
-            condicionCierre = true
-            alert("Hasta luego")
-            break;
-        default:
-            alert("El valor ingresado no es valido, por favor ingrese un numero del 1 al 5")
-            break;
-    }
-} */
 
 let nombre = prompt("Ingrese aquí su nombre:");
 let condicionCierre = false;
@@ -35,9 +8,7 @@ while (!condicionCierre) {
 Las funciones permitidas son:
 1 - Cambiar Nombre
 2 - Calcular que aumento te corrresponde
-3 - Realizar función 3
-4 - Realizar función 4
-5 - Finalizar`));
+3 - Finalizar`));
 
     switch (eleccion) {
         case 1:
@@ -50,19 +21,11 @@ Las funciones permitidas son:
             aumento();
             break;
         case 3:
-            // Llamar a la función 3
-            alert("Elegiste la función 3");
-            break;
-        case 4:
-            // Llamar a la función 4
-            alert("Elegiste la función 4");
-            break;
-        case 5:
-            condicionCierre = true;
-            alert("Hasta luego");
+            alert("Que sigas bien! espero verte de nuevo pronto!")
+            condicionCierre = true
             break;
         default:
-            alert("El valor ingresado no es válido, por favor ingrese un número del 1 al 5");
+            alert("El valor ingresado no es válido, por favor ingrese un número del 1 al 3");
             break;
     }
 }
@@ -83,7 +46,7 @@ function aumento() {
 
     let nuevoSueldo = sueldoActual;
 
-    for (let i = 11; i >= (12 - mesesSinaumento); i--) {
+    for (let i = 11; i >= (13 - mesesSinaumento); i--) {
         let tasaDecimal = inflacion[i] / 100;
         nuevoSueldo = (1 + tasaDecimal) * nuevoSueldo;
         console.log(`indice:${i} , interes${nuevoSueldo}`);
