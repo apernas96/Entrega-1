6fe9f900a73b18cc26c063b38bf31de7c2988697
Andres Pernas <ing.andrespernas@gmail.com>
2023-08-24 01:05:01 -0300

    Gestor gastos anda bien, para ver los gastos falla

--- JS/index.js
+++ JS/index.js
@@ -1,52 +1,57 @@
-
-
 let nombre = prompt("Ingrese aquí su nombre:");
 let condicionCierre = false;
-let gastos = [];
+let gastosArray = [];
 
-while (!condicionCierre) {
-    let eleccion = parseInt(prompt(`Hola ${nombre}, ¿en qué puedo ayudarte hoy?
+function menuPrincipal() {
+    while (!condicionCierre) {
+        let eleccion = parseInt(prompt(`Hola ${nombre}, ¿en qué puedo ayudarte hoy?
 Las funciones permitidas son:
 1 - Calcular tu aumento por inflacion
 2 - Gestor de gastos
 3 - Mostrar gastos registrados
 4 - Finalizar`));
 
-    switch (eleccion) {
-        case 1:
-            // Llamar a la función 2
-            alert("Elegiste la función 1 - Calculadora de aumento");
-            aumento();
-            break;
+        console.log("Opcion seleccionada:", eleccion);
 
-        case 2: // Agregar gastos
-            alert("Elegiste la función 2 - Gestor de gastos");
-            gestorGastos(gastos);
-            break;
-        case 3: // Mostrar gastos registrados
-            alert("Elegiste la función 3 - Mostrar gastos registrados");
-            mostrarGastos(gastos);
-            break;
-        case 4: //Finalizar la ejecucion
-            alert("Que sigas bien! espero verte de nuevo pronto!")
-            condicionCierre = true
-            break;
+        switch (eleccion) {
+            case 1:
+                console.log("Opcion seleccionada: aumento");
+                aumento();
+                break;
 
-        default: //Validacion de datos
-            alert("El valor ingresado no es válido, por favor ingrese un número del 1 al 4");
-            break;
+            case 2:
+                console.log("Opcion seleccionada: gestorGastos");
+                gestorGastos();
+                break;
+
+            case 3:
+                console.log("Opcion seleccionada: mostrarGastos");
+                mostrarGastos(gastosArray);
+                break;
+
+            case 4:
+                console.log("Opcion seleccionada: Finalize");
+                alert("Que sigas bien! Espero verte de nuevo pronto!");
+                condicionCierre = true;
+                break;
+
+            default:
+                alert("El valor ingresado no es válido, por favor ingrese un número del 1 al 4");
+                break;
+        }
     }
 }
-
-
-// Logica del Case 1 del switch
+menuPrincipal();
 function aumento() {
     let sueldoActual = parseFloat(prompt("Ingrese su sueldo actual:"));
     let mesesSinaumento;
     const inflacion = [8, 7, 6, 9, 11, 10, 8, 5, 7, 13, 15, 9];
+
     do {
         mesesSinaumento = parseInt(prompt("Cuantos meses hace que no te aumetan?"));
-        if (mesesSinaumento > 12 || mesesSinaumento <= 0) { alert("Lo siento, solo podes ingresar un numero de meses entre 1 y 12") };
+        if (mesesSinaumento > 12 || mesesSinaumento <= 0) {
+            alert("Lo siento, solo podes ingresar un numero de meses entre 1 y 12");
+        }
     }
     while (mesesSinaumento > 12 || mesesSinaumento <= 0);
 
@@ -61,121 +66,125 @@ function aumento() {
 }
 
 
-// Aca empieza la funcion 3 agregada para la segunda entrega
-// function obtenerMetodoPago() {
-//     let metodoPago;
-//     // QUIERO OBTENER UN NUMERO VALIDO
-//     do {
-//         metodoPago = parseInt(prompt("Ingrese el método de pago: \n 1 - Efectivo \n 2 - Debito \n 3 - Credito \n 4 - Otro"));
-//         if (!(metodoPago >= 1 && metodoPago <= 4)) {
-//             alert("Lo siento, solo podes ingresar un numero entre 1 y 4");
-//         };
-//     }
-//     while (!(metodoPago >= 1 && metodoPago <= 4));
+function gestorGastos() {
+    let correr = true
+    while (correr) {
+        let descripcion = prompt("Ingrese la descripción de la compra:");
+        let monto = parseFloat(prompt("Ingrese el monto de la compra:"));
+        let metodoPago = obtenerMetodoPago();
+        let fecha = new Date();
+        let gasto = {
+            descripcion: descripcion,
+            monto: monto,
+            metodo: metodoPago,
+            fecha: fecha,
+        }
 
-//     switch (metodoPago) {
-//         case 1:
-//             return "Efectivo";
-//             break;
-//         case 2:
-//             return "Debito";
-//             break;
-//         case 3:
-//             return "Credito";
-//             break;
-//         default:
-//             return "Otro";
-//             break;
-//     }
+        gastosArray.push(gasto);
+        console.log(gastosArray);
+        correr = prompt(`Quieres agregar otro gasto?
+        Si - Ingresar otro gasto
+        No - Volver al menu principal`).toLowerCase();
 
-// }
-const obtenerMetodoPago = () => {
+        if (correr === 'si') { correr = true; }
+        else if (correr === 'no') {
+            correr = false;
+            console.log("Volver al menu principal ");
+            menuPrincipal();
+        }
+        else {
+            alert("Lo siento, solo puedes ingresar si o no");
+        }
+    }
+}
+function obtenerMetodoPago() {
     let metodoPago;
-    debugger
     do {
         metodoPago = parseInt(prompt("Ingrese el método de pago: \n 1 - Efectivo \n 2 - Debito \n 3 - Credito \n 4 - Otro"));
         if (!(metodoPago >= 1 && metodoPago <= 4)) {
-            alert("Lo siento, solo podes ingresar un numero entre 1 y 4")
-        };
-    }
-    while (!(metodoPago >= 1 && metodoPago <= 4));
+            alert("Lo siento, solo puedes ingresar un número entre 1 y 4");
+        }
+    } while (!(metodoPago >= 1 && metodoPago <= 4));
 
     switch (metodoPago) {
         case 1:
             return "Efectivo";
-            break;
         case 2:
             return "Debito";
-            break;
         case 3:
             return "Credito";
-            break;
         default:
             return "Otro";
-            break;
-    }
-
-    return metodoPago;
-}
-function gestorGastos(gastosArray) {
-    //volver tiene que ser false para que entre la primera vez
-    // let volver = false;
-    // while (!volver) {
-    let descripcion = prompt("Ingrese la descripción de la compra:");
-    let monto = parseFloat(prompt("Ingrese el monto de la compra:"));
-    let metodoPago = obtenerMetodoPago();
-    let fecha = new Date();
-
-    let gasto = {
-        descripcion: descripcion,
-        monto: monto,
-        metodo: metodoPago,
-        fecha: fecha,
-    }
-
-    gastosArray.push(gasto);
-    // break;
-}
-let opcion = parseInt(prompt(`Quieres volver al menu principal o agregar otro gasto?
-1 - Ingresar otro gasto
-2 - Volver al menu principal`));
-
-if (opcion === 2) {
-    volver = true;
-}
-
-
-//codigo despues
-
-
-
-console.log("Gasto registrado:", gasto);
-
-class Persona {
-    constructor(nombre, sueldo) {
-        this.nombre = nombre;
-        this.edad = sueldo;
     }
 }
-const persona = new Persona(nombre, sueldoActual);
-//esto se ejecuta solo despues del while de condicion cierre
-let sueldoActual = parseInt(prompt("Ingrese su sueldo actual:"));
 
-
+menuPrincipal();
 function mostrarGastos(gastos) {
-    console.log("Lista de gastos:");
-    let volverAlMenu = confirm("¿Deseas volver al menú principal?");
-    if (volverAlMenu) {
-        return; // Salir de la función y volver al menú principal
+    if (gastos.length === 0) {
+        alert("No hay gastos registrados. Agrega gastos para ver la información.");
+        return; // Salir de la función si no hay gastos
     }
 
-    for (let i = 0; i < gastosArray.length; i++) {
-        let gasto = gastosArray[i];
+    console.log("Lista de todos los gastos:");
+    for (let i = 0; i < gastos.length; i++) {
+        let gasto = gastos[i];
         console.log(`Descripción: ${gasto.descripcion}`);
         console.log(`Monto: ${gasto.monto}`);
         console.log(`Método de pago: ${gasto.metodo}`);
         console.log(`Fecha: ${gasto.fecha}`);
         console.log("----");
     }
+
+    let opcionesFiltro = [
+        "Filtrar por descripción",
+        "Mostrar todos los gastos",
+        "Volver al menú principal"
+    ];
+
+    let opcionSeleccionada = prompt(`Opciones de filtrado:\n\n1 - ${opcionesFiltro[0]}\n2 - ${opcionesFiltro[1]}\n3 - ${opcionesFiltro[2]}`);
+
+    switch (opcionSeleccionada) {
+        case "1":
+            let descripcionBusqueda = prompt("Ingrese la descripción para filtrar los gastos:");
+            mostrarGastosFiltrados(gastos, descripcionBusqueda);
+            break;
+
+        case "2":
+            console.log("Mostrar todos los gastos:");
+            for (let i = 0; i < gastos.length; i++) {
+                let gasto = gastos[i];
+                console.log(`Descripción: ${gasto.descripcion}`);
+                console.log(`Monto: ${gasto.monto}`);
+                console.log(`Método de pago: ${gasto.metodo}`);
+                console.log(`Fecha: ${gasto.fecha}`);
+                console.log("----");
+            }
+            break;
+
+        default:
+            alert("Volviendo al menú principal.");
+            break;
+    }
 }
 
+function mostrarGastosFiltrados(gastos, descripcionBusqueda) {
+    let gastosFiltrados = gastos.filter(gasto => gasto.descripcion.toLowerCase().includes(descripcionBusqueda.toLowerCase()));
+
+    if (gastosFiltrados.length === 0) {
+        alert(`No se encontraron gastos para la descripción "${descripcionBusqueda}".`);
+        return; // Salir de la función si no hay gastos filtrados
+    }
+
+    let mensaje = `Lista de gastos para la descripción "${descripcionBusqueda}":\n\n`;
+
+    for (let i = 0; i < gastosFiltrados.length; i++) {
+        let gasto = gastosFiltrados[i];
+        mensaje += `Descripción: ${gasto.descripcion}\n`;
+        mensaje += `Monto: ${gasto.monto}\n`;
+        mensaje += `Método de pago: ${gasto.metodo}\n`;
+        mensaje += `Fecha: ${gasto.fecha}\n`;
+        mensaje += "----\n";
+    }
+
+    alert(mensaje);
+}
